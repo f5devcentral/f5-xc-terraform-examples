cedb13d470410c28f0fd475e457185cfa29c33ec
Mohammed Janibasha <jani.basha.5000@gmail.com>
2023-08-28 23:08:27 +0530

    Update providers.tf

--- workflow-guides/waf/f5-xc-waf-on-k8s/booksinfo/providers.tf
+++ workflow-guides/waf/f5-xc-waf-on-k8s/booksinfo/providers.tf
@@ -6,5 +6,4 @@ provider "kubernetes" {
     host = local.host
     cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
     token = data.aws_eks_cluster_auth.auth.token
-    load_config_file = false
 }
