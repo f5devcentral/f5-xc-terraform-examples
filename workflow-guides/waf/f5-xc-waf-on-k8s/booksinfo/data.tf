f28b8ccc74e9de8e1db38ac496cba61cf8979373
Janibasha <m.janibasha@f5.com>
2023-08-28 20:16:57 +0530

    removed nap and nic files

--- workflow-guides/waf/f5-xc-waf-on-k8s/booksinfo/data.tf
+++ workflow-guides/waf/f5-xc-waf-on-k8s/booksinfo/data.tf
@@ -6,16 +6,7 @@ data "tfe_outputs" "eks" {
   organization = var.tf_cloud_organization
   workspace = "eks"
 }
-data "tfe_outputs" "nap" {
-  count = data.tfe_outputs.infra.values.nap ? 1 : 0
-  organization = var.tf_cloud_organization
-  workspace = "nap"
-}
-data "tfe_outputs" "nic" {
-  count = data.tfe_outputs.infra.values.nic ? 1 : 0
-  organization = var.tf_cloud_organization
-  workspace = "nic"
-}
+
 data "aws_eks_cluster_auth" "auth" {
   name = data.tfe_outputs.eks.values.cluster_name
 }
