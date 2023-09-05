f9a1d499137ab96209d29ca2cc85fa34422781d6
Mohammed Janibasha <jani.basha.5000@gmail.com>
2023-09-05 16:27:32 +0530

    Update outputs.tf

--- workflow-guides/waf/f5-xc-waf-on-k8s/terraform/xc/outputs.tf
+++ workflow-guides/waf/f5-xc-waf-on-k8s/terraform/xc/outputs.tf
@@ -1,5 +1,5 @@
 output "xc_lb_name" {
-  value = nonsensitive(volterra_http_loadbalancer.lb_https.name)
+  value = nonsensitive(volterra_http_loadbalancer.k8s-site-demo.name)
 }
 output "xc_waf_name" {
   value = nonsensitive(volterra_app_firewall.waap-tf.name)
