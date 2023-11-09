31a9511d07c92a11d7458ef4f49458fae4834b8c
Kyle Roberts <96107094+karlbort@users.noreply.github.com>
2023-11-08 23:50:18 -1000

    Update curl-stuff copy.sh

--- workflow-guides/bot/deploy-botdefense-against-automated-threats-on-regional-edges-with-f5xc/validation-tools/curl-stuff copy.sh
+++ workflow-guides/bot/deploy-botdefense-against-automated-threats-on-regional-edges-with-f5xc/validation-tools/curl-stuff copy.sh
@@ -1,6 +1,6 @@
 for run in {1..10}
 do
- curl -s 'https://community.f5.com/t5/technical-articles/protecting-your-native-mobile-apps-with-f5-xc-mobile-app-shield/ta-p/319311' -i -X POST -d "username=1&password=1" 
+ curl -s 'http://ves-io-your-domain.ac.vh.ves.io/login' -i -X POST -d "username=1&password=1" 
  echo
  echo CURL Credential Stuffing attempt $run done
  sleep 2
