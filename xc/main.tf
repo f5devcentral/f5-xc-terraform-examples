f491dc992f8bf2f0b006864d4599e30716ce822b
Shubham Mishra <90624610+shubhammishra9@users.noreply.github.com>
2023-12-04 17:21:24 +0530

    Update main.tf

--- xc/main.tf
+++ xc/main.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
   features {}
-
+  count = var.azure ? 1 : 0
   subscription_id   = "${var.azure_subscription_id}"
   tenant_id         = "${var.azure_subscription_tenant_id}"
   client_id         = "${var.azure_service_principal_appid}"
