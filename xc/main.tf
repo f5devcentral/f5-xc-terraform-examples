265fadd03b18b9b68873c4d7d46dde0287468cde
Mohammed Janibasha <jani.basha.5000@gmail.com>
2023-12-04 20:58:16 +0530

    Update main.tf

--- xc/main.tf
+++ xc/main.tf
@@ -1,9 +1,5 @@
 provider "azurerm" {
   features {}
-  subscription_id   = "${var.azure_subscription_id}"
-  tenant_id         = "${var.azure_subscription_tenant_id}"
-  client_id         = "${var.azure_service_principal_appid}"
-  client_secret     = "${var.azure_service_principal_password}"
 }
 
 provider "volterra" {
