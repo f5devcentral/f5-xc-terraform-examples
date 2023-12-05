f81f76f0ce2c7ffc877f1b0a9fc7cb8bad39e4c7
Mohammed Janibasha <jani.basha.5000@gmail.com>
2023-12-05 10:34:45 +0530

    removed duplicate code

--- xc/eks_ce_registration.tf
+++ xc/eks_ce_registration.tf
@@ -1,5 +1,14 @@
+# temporary file which is needed for site cleanup
+resource "volterra_site_state" "site" {
+  count = var.eks_ce_site ? 1 : 0
+  name  = var.site_name
+  state = "DECOMMISSIONING"
+  when  = "delete"
+}
+
 resource "volterra_registration_approval" "k8s-ce" {
   count = var.eks_ce_site ? 1 : 0
+  depends_on      =  [volterra_site_state.site]
   cluster_name  = var.site_name
   cluster_size  = 1
   retry = 5
