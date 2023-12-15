adafa5ecc30deeefb2e360d8eff9c3b05075258a
Shubham Mishra <90624610+shubhammishra9@users.noreply.github.com>
2023-12-15 17:52:18 +0530

    Update variables.tf

--- azure/azure-vm/variables.tf
+++ azure/azure-vm/variables.tf
@@ -4,17 +4,29 @@ variable "tf_cloud_organization" {
 }
 
 variable "azure_subscription_id" {
-  type    = string
+  description = "Azure Subscription ID"
+  type        = string
+  sensitive   = true
+  default     = null
 }
 
 variable "azure_subscription_tenant_id" {
-  type    = string
+  description = "Azure Tenant ID"
+  type        = string
+  sensitive   = true
+  default     = null
 }
 
 variable "azure_service_principal_appid" {
-  type    = string
+  description = "Azure Client ID"
+  type        = string
+  sensitive   = true
+  default     = null
 }
 
 variable "azure_service_principal_password" {
-  type    = string
+  description = "Azure Client Secret"
+  type      = string
+  sensitive = true
+  default   = null
 }
