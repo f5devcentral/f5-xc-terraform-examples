e4333b17a9ccfa044ca6145eca3f42e4d75742d0
Janibasha <m.janibasha@f5.com>
2024-04-20 11:58:12 +0530

    Update xc_loadbalancer.tf

--- xc/xc_loadbalancer.tf
+++ xc/xc_loadbalancer.tf
@@ -373,7 +373,7 @@ resource "volterra_http_loadbalancer" "lb_https" {
           flow_label {
             authentication {
               login {
-                disable_transaction_result { }
+                disable_transaction_result = true
               }
             }
           }
