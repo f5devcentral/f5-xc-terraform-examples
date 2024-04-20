4fd50202d7b664304ec9e63aa13daabaca4a1026
Janibasha <m.janibasha@f5.com>
2024-04-20 11:31:29 +0530

    Update outputs.tf

--- xc/outputs.tf
+++ xc/outputs.tf
@@ -1,8 +1,8 @@
 output "xc_lb_name" {
-  value = nonsensitive(volterra_http_loadbalancer.lb_https.name)
+  value = volterra_http_loadbalancer.lb_https.name
 }
 output "xc_waf_name" {
-  value = nonsensitive(volterra_app_firewall.waap-tf.name)
+  value = volterra_app_firewall.waap-tf.name
 }
 output "endpoint" {
   value = var.app_domain
