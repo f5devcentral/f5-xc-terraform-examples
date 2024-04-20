ff6f0ab597acd67556100ef71889d0ea17c22182
Janibasha <m.janibasha@f5.com>
2024-04-20 14:46:30 +0530

    cname

--- xc/outputs.tf
+++ xc/outputs.tf
@@ -10,6 +10,6 @@ output "endpoint" {
 output "az_ce_site_pub_ip" {
   value = var.az_ce_site ? regex("master_public_ip_address = \"((?:\\d{1,3}\\.){3}\\d{1,3})\"", volterra_tf_params_action.action_apply[0].tf_output) : null
 }
-output "lb_domain" {
-  value = volterra_http_loadbalancer.lb_https.domains
+output "lb_cname" {
+  value = volterra_http_loadbalancer.lb_https.cname
 }
