8e3513307fb1e91fd092b9269a33ce4e2057d4c9
Janibasha <m.janibasha@f5.com>
2024-04-20 10:33:02 +0530

    infra

--- xc/data.tf
+++ xc/data.tf
@@ -5,27 +5,27 @@ data "tfe_outputs" "infra" {
 }
 
 data "tfe_outputs" "bigip" {
-  count = data.tfe_outputs.infra.values.bigip ? 1 : 0
+  count = data.tfe_outputs.infra.0.values.bigip ? 1 : 0
   organization = var.tf_cloud_organization
   workspace = "bigip-base"
 }
 data "tfe_outputs" "nap" {
-  count = data.tfe_outputs.infra.values.nap ? 1 : 0
+  count = data.tfe_outputs.infra.0.values.nap ? 1 : 0
   organization = var.tf_cloud_organization
   workspace = "nap"
 }
 data "tfe_outputs" "nic" {
-  count = data.tfe_outputs.infra.values.nic ? 1 : 0
+  count = data.tfe_outputs.infra.0.values.nic ? 1 : 0
   organization = var.tf_cloud_organization
   workspace = "nic"
 }
 data "tfe_outputs" "aks-cluster" {
-  count = data.tfe_outputs.infra.values.aks-cluster ? 1 : 0 
+  count = data.tfe_outputs.infra.0.values.aks-cluster ? 1 : 0
   organization = var.tf_cloud_organization
   workspace = "aks-cluster"
 }
 data "tfe_outputs" "azure-vm" {
-  count = data.tfe_outputs.infra.values.azure-vm ? 1 : 0 
+  count = data.tfe_outputs.infra.0.values.azure-vm ? 1 : 0
   organization = var.tf_cloud_organization
   workspace = "azure-vm"
 }
