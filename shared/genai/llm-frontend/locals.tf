2772883b51a775f1de4d468c87a4285e1b986dab
Janibasha <m.janibasha@f5.com>
2024-05-28 15:11:13 +0530

    Update locals.tf

--- shared/genai/llm-frontend/locals.tf
+++ shared/genai/llm-frontend/locals.tf
@@ -3,6 +3,6 @@ locals {
   project_prefix         = data.tfe_outputs.infra.values.project_prefix
   host                   = data.tfe_outputs.eks.values.kubernetes_cluster_host
   region                 = data.tfe_outputs.infra.values.gcp_region
-  cluster_ca_certificate = data.tfe_outputs.eks.values.kubernetes_cluster_ca_certificate-authority-data
+  cluster_ca_certificate = data.tfe_outputs.eks.values.kubernetes_cluster_ca_certificate
   cluster_name           = data.tfe_outputs.eks.values.kubernetes_cluster_name
 }
