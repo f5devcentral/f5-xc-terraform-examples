76dc731221be56031805f1796889405a9072670a
Mohammed Janibasha <jani.basha.5000@gmail.com>
2024-07-02 22:52:44 +0530

    Update xc_loadbalancer.tf

--- xc/xc_loadbalancer.tf
+++ xc/xc_loadbalancer.tf
@@ -450,6 +450,7 @@ resource "volterra_http_loadbalancer" "lb_https" {
   }
 
   dynamic "more_option" {
+    for_each = var.xc_namespace ? [1] : []
       content {
         idle_timeout = 300000
       }
