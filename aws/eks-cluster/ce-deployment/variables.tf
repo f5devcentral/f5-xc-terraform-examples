a3767662e7cc5babb5fe997ef7630babfd8956ab
Mohammed Janibasha <jani.basha.5000@gmail.com>
2024-07-02 11:54:10 +0530

    Update variables.tf

--- aws/eks-cluster/ce-deployment/variables.tf
+++ aws/eks-cluster/ce-deployment/variables.tf
@@ -3,3 +3,9 @@ variable "tf_cloud_organization" {
   type        = string
   description = "TF cloud org (Value set in TF cloud)"
 }
+
+variable "aws_waf_ce" {
+  description = "Infra"
+  type        = string
+  default     = ""
+}
