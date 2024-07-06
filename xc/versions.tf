bc43e1c8b52e5dd946544fc23f38b23b9abd162e
Mohammed Janibasha <jani.basha.5000@gmail.com>
2024-07-06 11:41:06 +0530

    Update versions.tf

--- xc/versions.tf
+++ xc/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     volterra = {
       source = "volterraedge/volterra"
-      version = "= 0.11.34"
+      version = ">= 0.11.34"
     }
       azurerm = {
       source  = "hashicorp/azurerm"
