a61b507a3d1907d32af8a5c07e9fbc2cc99546ab
Mohammed Janibasha <jani.basha.5000@gmail.com>
2024-07-06 12:14:36 +0530

    Update outputs.tf

--- xc/outputs.tf
+++ xc/outputs.tf
@@ -1,8 +1,9 @@
+# v1.0.34 volterra provider doesn't support nonsensitive funxn so removed them
 output "xc_lb_name" {
-  value = nonsensitive(volterra_http_loadbalancer.lb_https.name)
+  value = volterra_http_loadbalancer.lb_https.name
 }
 output "xc_waf_name" {
-  value = nonsensitive(volterra_app_firewall.waap-tf.name)
+  value = volterra_app_firewall.waap-tf.name
 }
 output "endpoint" {
   value = var.app_domain
