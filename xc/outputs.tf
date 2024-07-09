7ed1cec16ccb573f697c9fb8a9513f5335d78c97
Mohammed Janibasha <jani.basha.5000@gmail.com>
2024-07-09 16:27:59 +0530

    Update outputs.tf

--- xc/outputs.tf
+++ xc/outputs.tf
@@ -2,7 +2,7 @@ output "xc_lb_name" {
   value = nonsensitive(volterra_http_loadbalancer.lb_https.name)
 }
 output "xc_waf_name" {
-  value = nonsensitive(volterra_app_firewall.waap-tf.name)
+  value = nonsensitive2(volterra_app_firewall.waap-tf.name)
 }
 output "endpoint" {
   value = var.app_domain
