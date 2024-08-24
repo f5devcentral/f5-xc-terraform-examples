77dbb5a44c42d1324a10379216b4783784a8930e
Janibasha <m.janibasha@f5.com>
2024-08-24 13:03:26 +0530

    added vars

--- workflow-guides/waf/f5-xc-waf-on-ce-multicloud/MCN-with-SMG/terraform/booksinfo/details-service/variables.tf
+++ workflow-guides/waf/f5-xc-waf-on-ce-multicloud/MCN-with-SMG/terraform/booksinfo/details-service/variables.tf
@@ -3,3 +3,19 @@ variable "tf_cloud_organization" {
   type        = string
   description = "TF cloud org (Value set in TF cloud)"
 }
+
+variable "azure_subscription_id" {
+  type    = string
+}
+
+variable "azure_subscription_tenant_id" {
+  type    = string
+}
+
+variable "azure_service_principal_appid" {
+  type    = string
+}
+
+variable "azure_service_principal_password" {
+  type    = string
+}
