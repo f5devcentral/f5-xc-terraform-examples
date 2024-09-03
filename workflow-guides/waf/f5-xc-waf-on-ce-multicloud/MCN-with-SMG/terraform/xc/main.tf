507ad35b3d28738f9a4bbdee3ecf8e6355e1aba7
Janibasha <m.janibasha@f5.com>
2024-09-03 22:36:19 +0530

    Update main.tf

--- workflow-guides/waf/f5-xc-waf-on-ce-multicloud/MCN-with-SMG/terraform/xc/main.tf
+++ workflow-guides/waf/f5-xc-waf-on-ce-multicloud/MCN-with-SMG/terraform/xc/main.tf
@@ -5,6 +5,7 @@ provider "volterra" {
 provider "google" {
   project                 = local.gcp_project_id
   region                  = local.gcp_region
+  credentials             = var.GOOGLE_CREDENTIALS
 }
 
 provider "kubernetes" {
