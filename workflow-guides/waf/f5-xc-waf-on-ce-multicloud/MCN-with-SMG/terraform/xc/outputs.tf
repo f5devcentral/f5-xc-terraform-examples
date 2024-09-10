e3dcc3ef1eeed35308155abc0323cf83ea70d2e7
Janibasha <m.janibasha@f5.com>
2024-09-10 23:28:07 +0530

    code

--- workflow-guides/waf/f5-xc-waf-on-ce-multicloud/MCN-with-SMG/terraform/xc/outputs.tf
+++ workflow-guides/waf/f5-xc-waf-on-ce-multicloud/MCN-with-SMG/terraform/xc/outputs.tf
@@ -1,3 +1,7 @@
 output "endpoint" {
   value = var.app_domain
 }
+
+output "gcp_ce_privateip" {
+  value = local.gcp_ce_ip.0
+}
--- workflow-guides/waf/f5-xc-waf-on-ce-multicloud/MCN-with-SMG/terraform/xc/productpage.tf
+++ workflow-guides/waf/f5-xc-waf-on-ce-multicloud/MCN-with-SMG/terraform/xc/productpage.tf
@@ -50,7 +50,7 @@ resource "kubernetes_deployment" "productpage_v1" {
         }
 
         host_aliases {
-          ip        = local.gcp_ce_ip
+          ip        = local.gcp_ce_ip.0
           hostnames = ["details"]
         }
 
