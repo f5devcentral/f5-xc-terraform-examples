eebe56a0b8d5018b1d90ec706de0ffdf447536cf
kamarajugadda <salini2579@gmail.com>
2024-10-04 12:03:40 +0530

    add pub file

--- azure/bigip/bigip.tf
+++ azure/bigip/bigip.tf
@@ -11,7 +11,7 @@ module "bigip" {
   prefix                      = local.project_prefix
   resource_group_name         = local.resource_group_name
   f5_ssh_publickey            = azurerm_ssh_public_key.f5_key.public_key
-  mgmt_subnet_ids             = [{ "subnet_id" = local.subnet_name, "public_ip" = true, "private_ip_primary" = "" }]
+  mgmt_subnet_ids             = [{ "subnet_id" = local.subnet_id, "public_ip" = true, "private_ip_primary" = "" }]
   mgmt_securitygroup_ids      = [module.mgmt-network-security-group.network_security_group_id]
 #  availability_zone           = var.availability_zone
 #  availabilityZones_public_ip = var.availabilityZones_public_ip
