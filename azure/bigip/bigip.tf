8657e3d717716398a7b4c081dcd7845db3ebf6c4
kamarajugadda <salini2579@gmail.com>
2024-10-07 12:28:37 +0530

    pub changes

--- azure/bigip/bigip.tf
+++ azure/bigip/bigip.tf
@@ -1,3 +1,25 @@
+module "mgmt-network-security-group" {
+  source              = "Azure/network-security-group/azurerm"
+  resource_group_name = local.resource_group_name
+  security_group_name = format("%s-mgmt-nsg-%s", local.project_prefix, local.build_suffix)
+
+}
+
+resource "azurerm_network_security_rule" "mgmt_allow_rules" {
+  name                        = "Allow_Rules"
+  priority                    = 200
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_ranges      = ["8443","443","80","22"]
+  destination_address_prefix  = "*"
+  source_address_prefixes     = ["0.0.0.0/0"]
+  resource_group_name         = local.resource_group_name
+  network_security_group_name = format("%s-mgmt-nsg-%s", local.project_prefix, local.build_suffix)
+  depends_on                  = [module.mgmt-network-security-group]
+}
+
 resource "azurerm_ssh_public_key" "f5_key" {
   name                = format("%s-pubkey-%s", local.project_prefix, local.build_suffix)
   resource_group_name = local.resource_group_name
@@ -31,24 +53,3 @@ module "bigip" {
 #  depends_on = [module.bigip.onboard_do]
 #}
 
-module "mgmt-network-security-group" {
-  source              = "Azure/network-security-group/azurerm"
-  resource_group_name = local.resource_group_name
-  security_group_name = format("%s-mgmt-nsg-%s", local.project_prefix, local.build_suffix)
-
-}
-
-resource "azurerm_network_security_rule" "mgmt_allow_rules" {
-  name                        = "Allow_Rules"
-  priority                    = 200
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_ranges      = ["8443","443","80","22"]
-  destination_address_prefix  = "*"
-  source_address_prefixes     = ["0.0.0.0/0"]
-  resource_group_name         = local.resource_group_name
-  network_security_group_name = format("%s-mgmt-nsg-%s", local.project_prefix, local.build_suffix)
-  depends_on                  = [module.mgmt-network-security-group]
-}
