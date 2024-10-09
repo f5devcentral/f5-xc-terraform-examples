3c57f3c7671404f6220d4d683ac6ce8bd573e724
yoctoalex <shemyakin@yoctocode.com>
2024-10-09 05:25:39 +0200

    bump aws-vpc-site module version to v0.0.11

--- f5/xc/aws-vpc-site/aws-vpc-site.tf
+++ f5/xc/aws-vpc-site/aws-vpc-site.tf
@@ -1,6 +1,6 @@
 module "aws_vpc_site" {
   source                                   = "f5devcentral/aws-vpc-site/xc"
-  version                                  = "0.0.10"
+  version                                  = "0.0.11"
   site_name                                = ("" != var.prefix) ? format("%s-%s", var.prefix, var.name) : var.name
   aws_region                               = var.aws_region
   aws_cloud_credentials_name               = var.aws_cloud_credentials_name
