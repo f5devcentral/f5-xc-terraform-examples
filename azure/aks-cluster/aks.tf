81472bead9c77b05eadbd611911034f13be7dd8b
kamarajugadda <salini2579@gmail.com>
2025-02-11 15:50:40 +0530

    role assignment code

--- azure/aks-cluster/aks.tf
+++ azure/aks-cluster/aks.tf
@@ -21,22 +21,53 @@ resource "azurerm_kubernetes_cluster" "ce_waap" {
 	network_plugin = "azure"
   }
 }
-
-data "azuread_service_principal" "aks-sp" {
-  display_name  = azurerm_kubernetes_cluster.ce_waap.name
+#data "azurerm_user_assigned_identity" "aks-identity" {
+#  name                = "${azurerm_kubernetes_cluster.kubernetes_cluster[0].name}-agentpool"
+#  resource_group_name = "MC_${module.resource_group_kubernetes_cluster[0].name}_aks-prjx-spoke-dev-eastus-001_eastus"
+#
+#  depends_on          = [module.resource_group_kubernetes_cluster]
+#}
+#
+## Provide ACR Pull permission to AKS SystemAssigned Identity
+#resource "azurerm_role_assignment" "acrpull_role" {
+#  scope                            = module.container_registry[0].id
+#  role_definition_name             = "AcrPull"
+#  principal_id                     = data.azurerm_user_assigned_identity.aks-identity.principal_id
+#  skip_service_principal_aad_check = true
+#
+#  depends_on                       = [
+#    data.azurerm_user_assigned_identity.aks-identity
+#  ]
+#}
+#
+#resource "azurerm_role_assignment" "aks_id_network_contributor_subnet" {
+#  scope                = data.azurerm_subnet.aks-subnet.id
+#  role_definition_name = "Network Contributor"
+#  principal_id         = data.azurerm_user_assigned_identity.aks-identity.principal_id
+#
+#  depends_on = [data.azurerm_user_assigned_identity.aks-identity]
+#}
+#
+#data "azuread_service_principal" "aks-sp" {
+#  display_name  = azurerm_kubernetes_cluster.ce_waap.name
+#  depends_on = [azurerm_kubernetes_cluster.ce_waap]
+#}
+#
+#resource "azurerm_role_assignment" "network_contributor_subnet" {
+#  scope                = local.subnet_id
+#  role_definition_name = "Network Contributor"
+#  principal_id         = data.azuread_service_principal.aks-sp.object_id
+#
+#  depends_on = [data.azuread_service_principal.aks-sp]
+#}
+resource "azurerm_role_assignment" "network-contributor" {
+  principal_id         = azurerm_kubernetes_cluster.ce_waap.identity.principal_id
+  role_definition_name = "Network Contributor"
+  scope                = local.subnet_id
   depends_on = [azurerm_kubernetes_cluster.ce_waap]
 }
-
-resource "azurerm_role_assignment" "network_contributor_subnet" {
-  scope                = local.subnet_id
-  role_definition_name = "Network Contributor"
-  principal_id         = data.azuread_service_principal.aks-sp.object_id
-
-  depends_on = [data.azuread_service_principal.aks-sp]
-}
-
 resource "local_file" "kubeconfig" {
-  depends_on   = [azurerm_role_assignment.network_contributor_subnet]
+  depends_on   = [azurerm_role_assignment.network-contributor]
   filename     = "./kubeconfig"
   content      = azurerm_kubernetes_cluster.ce_waap.kube_config_raw
 }
