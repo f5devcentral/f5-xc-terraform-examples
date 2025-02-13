548e2f394ab80cdf4157da997275c9e72f59a08d
kamarajugadda <salini2579@gmail.com>
2025-02-13 14:20:08 +0530

    configure bigip

--- azure/aks-cluster/data.tf
+++ azure/aks-cluster/data.tf
@@ -2,9 +2,6 @@ data "tfe_outputs" "azure-infra" {
   organization = var.tf_cloud_organization
   workspace = "azure-infra"
 }
-data "local_file" "out" {
-        filename = "./output.txt"
-}
 data "azurerm_lb" "lb" {
   name = "kubernetes-internal"
   resource_group_name = format("MC_%s-rg-%s_%s-aks-%s_%s", local.project_prefix, local.build_suffix,local.project_prefix, local.build_suffix,local.azure_region)
---
+++ azure/bigip/bot-defense/variables.tf
@@ -0,0 +1,66 @@
+variable "tf_cloud_organization" {
+  type = string
+  description = "TF cloud org (Value set in TF cloud)"
+}
+
+variable "azure_subscription_id" {
+  type    = string
+}
+
+variable "azure_subscription_tenant_id" {
+  type    = string
+}
+
+variable "azure_service_principal_appid" {
+  type    = string
+}
+
+variable "azure_service_principal_password" {
+  type    = string
+}
+
+variable "f5_bigip_image" {
+  type    = string
+  default = "f5-big-best-plus-hourly-25mbps"
+}
+
+variable "f5_bigip_password" {
+  type    = string
+  description = "BIGIP instance password"
+  default     = "Siddarth@12345"
+}
+# XC Bot Connector Variables
+variable "application_id" {
+  description = "XC Application ID"
+  type        = string
+  default     = ""
+}
+
+variable "tenant_id" {
+  description = "XC Tenant ID"
+  type        = string
+  default     = ""
+}
+
+variable "telemetry_header_prefix" {
+  description = "Telemetry Header Prefix"
+  type        = string
+  default     = ""
+}
+
+variable "api_key" {
+  description = "XC API Key"
+  type        = string
+  default     = ""
+}
+
+variable "availability_zone" {
+  description = "If you want the VM placed in an Azure Availability Zone, and the Azure region you are deploying to supports it, specify the number of the existing Availability Zone you want to use."
+  default     = 1
+}
+
+variable "availabilityZones_public_ip" {
+  description = "The availability zone to allocate the Public IP in. Possible values are Zone-Redundant, 1, 2, 3, and No-Zone."
+  type        = string
+  default     = "Zone-Redundant"
+}
