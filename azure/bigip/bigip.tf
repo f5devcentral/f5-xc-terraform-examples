7dd9b4b71b8421d2625f0909bc00b77132dfd944
kamarajugadda <salini2579@gmail.com>
2025-02-14 11:25:16 +0530

    locals

--- azure/bigip/bigip.tf
+++ azure/bigip/bigip.tf
@@ -41,18 +41,3 @@ module "bigip" {
   availability_zone           = var.availability_zone
   availabilityZones_public_ip = var.availabilityZones_public_ip
 }
-
-#resource "null_resource" "clusterDO" {
-#
-#  count = 1
-#
-#  provisioner "local-exec" {
-#    command = "cat > DO_1nic-instance${count.index}.json <<EOL\n ${module.bigip[count.index].onboard_do}\nEOL"
-#  }
-#  provisioner "local-exec" {
-#    when    = destroy
-#    command = "rm -rf DO_1nic-instance${count.index}.json"
-#  }
-#  depends_on = [module.bigip.onboard_do]
-#}
-
