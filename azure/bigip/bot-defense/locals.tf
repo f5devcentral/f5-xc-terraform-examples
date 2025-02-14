cda24cb6041f2bc3718616f981203cc8ce9b8365
kamarajugadda <salini2579@gmail.com>
2025-02-14 12:02:44 +0530

    pprivate+ip

--- azure/bigip/bot-defense/locals.tf
+++ azure/bigip/bot-defense/locals.tf
@@ -2,7 +2,7 @@ locals {
   bigip_public_ip = data.tfe_outputs.bigip.values.mgmtPublicIP[0]
   bigip_username  = data.tfe_outputs.bigip.values.bigip_username[0]
   bigip_password  = nonsensitive(data.tfe_outputs.bigip.values.bigip_password[0])
-  bigip_private   = data.tfe_outputs.bigip.values.bigip_private_addresses[2][0][0]
+  bigip_private_ip  = data.tfe_outputs.bigip.values.bigip_private_addresses[0]['mgmt_private']['private_ip'][0]
   port            = data.tfe_outputs.bigip.values.mgmtPort[0]
   app_ip          = data.tfe_outputs.aks-cluster.values.external_ip
 }
