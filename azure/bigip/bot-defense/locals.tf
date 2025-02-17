d39d867b9e2bfe676b070679577cab31f5e3df91
kamarajugadda <salini2579@gmail.com>
2025-02-17 15:30:38 +0530

    route_tables association

--- azure/bigip/bot-defense/locals.tf
+++ azure/bigip/bot-defense/locals.tf
@@ -4,5 +4,5 @@ locals {
   bigip_password  = nonsensitive(data.tfe_outputs.bigip.values.bigip_password[0])
   bigip_private_ip  = data.tfe_outputs.bigip.values.bigip_private_addresses[0]["mgmt_private"]["private_ip"][0]
   port            = data.tfe_outputs.bigip.values.mgmtPort[0]
-  app_ip          = data.tfe_outputs.aks-cluster.values.external_ip
+  app_ip          = data.tfe_outputs.aks-cluster.values.app_external_ip
 }
