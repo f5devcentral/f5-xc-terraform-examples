a1b4e0e3afc6ce10ebf21556cb90914057676014
drpotters <daverpotter2@gmail.com>
2025-03-26 11:22:42 -0700

    change deprecated skip_provider_registration to new resource_provider_registrations

--- f5/xc/azure-cloud-credentials/main.tf
+++ f5/xc/azure-cloud-credentials/main.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
   features {}
-  skip_provider_registration = "true"
+  resource_provider_registrations = "none"
 
   client_id       = var.azure_client_id
   client_secret   = var.azure_client_secret
--- workflow-guides/smcn/mcn-distributed-apps-l3/terraform/azure-aks/main.tf
+++ workflow-guides/smcn/mcn-distributed-apps-l3/terraform/azure-aks/main.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  skip_provider_registration = "true"
+  resource_provider_registrations = "none"
 
   client_id       = var.azure_client_id
   client_secret   = var.azure_client_secret
